99c458d39c58f38ddb7f4ba16b1e3c251b342cb3
Suhail Kv <suhailkv99@gmail.com>
2025-09-26 02:37:18 +0530

    minor

--- frontend/src/pages/expenses/Expenses.jsx
+++ frontend/src/pages/expenses/Expenses.jsx
@@ -3,7 +3,7 @@ import { Box } from "@mui/material";
 import { useExpenses } from "./hooks/useExpenses";
 import ExpenseToolbar from "./components/ExpenseToolbar";
 import ExpenseTable from "./components/ExpenseTable";
-import ExpenseForm from "../../components/expenses/ExpenseForm/ExpenseForm";
+import ExpenseForm from "../../components/expenses/expenseForm/ExpenseForm";
 
 const Expenses = () => {
     const [search, setSearch] = useState("");
